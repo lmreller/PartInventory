eb15a55b26ff67baa736492c48a9ae8dd2c3c6f0
lmreller <31704165+lmreller@users.noreply.github.com>
2017-10-25 23:17:05 -0500

    fix merge shgit

--- Development/app/src/main/java/com/partinventory/Model/Item.java
+++ Development/app/src/main/java/com/partinventory/Model/Item.java
@@ -12,16 +12,11 @@ public class Item {
     private int year;
     private float price = 0;
 
-<<<<<<< HEAD
-    public Item(){
-
-=======
     public Item(String Part_Name, String Part_Desc, int Part_Year, float Sell_Price){
         name = Part_Name;
         description = Part_Desc;
         year = Part_Year;
         price = Sell_Price;
->>>>>>> a7153cda96d6d67b22850ba21fa26479a2eb705c
     }
 
     private void genID(int id){
@@ -29,30 +24,26 @@ public class Item {
     }
 
     //GETTERS
-    public int getId(int id) {
+    public int getId() {
 
         return id;
     }
 
-    public String getName(String name) {
+    public String getName() {
 
         return name;
     }
 
-    public String getDescription(String description) {
+    public String getDescription() {
 
         return description;
     }
 
-    public int getYear(int year) {
+    public int getYear() {
         return year;
     }
 
-    public int getQuantity(int quantity) {
-        return quantity;
-    }
-
-    public float getPrice(int price) {
+    public float getPrice() {
         return price;
     }
 }
